166c735a0c1f87090a676651e450d63767991fdf
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-07 00:29:35 +0200

    create module for request urls

---
+++ src/constants/api.js
@@ -0,0 +1,13 @@
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+const LETTERA_FRONTPAGE_URL = `${BASE_URL}/frontpage`;
+const LETTERA_MOSTREAD_URL = `${BASE_URL}/mostread`;
+const LETTERA_LATEST_URL = `${BASE_URL}/latest`;
+const LETTERA_ARTICLE_URL = `${BASE_URL}/article`;
+
+module.exports = {
+  LETTERA_FRONTPAGE_URL,
+  LETTERA_MOSTREAD_URL,
+  LETTERA_LATEST_URL,
+  LETTERA_ARTICLE_URL
+};
