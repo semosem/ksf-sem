4f9f5c0bfc35cdad4945666ec307f9ea19cc5900
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-08 10:22:14 +0200

    App style improvement

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Body from "./Body";
+import classnames from "classnames";
 import "./article.scss";
-const Article = ({ article, handleLoadArticle }) => {
+const Article = ({ article, handleLoadArticle, isList, isArticleView }) => {
   if (!article) return null;
   const {
     publishingTime,
@@ -14,30 +15,52 @@ const Article = ({ article, handleLoadArticle }) => {
     uuid
   } = article;
 
+  const listImageClass = classnames("Article__image", {
+    "Article__image-rounded": isList
+  });
+  const articleTitle = classnames("title", {
+    list_title: isList
+  });
+
   return (
     <article className="Article" onClick={() => handleLoadArticle(uuid)}>
-      <header className="Article__header">
-        {tags[0] && <a href="">{tags[0].toUpperCase()}</a>}
-        <h2>{article.title}</h2>
-      </header>
+      {!isList && tags[0] && <a href="">{tags[0].toUpperCase()}</a>}
       {/* article main image*/}
+      {/* title should be above image when component is used on article view */}
+      {isArticleView && (
+        <header className="Article__header">
+          <h2 className={articleTitle}>{article.title}</h2>
+        </header>
+      )}
       {listImage && (
-        <figure className="Article__image">
+        <figure className={listImageClass}>
           <img src={listImage.url} alt="article main thumb" />
-          <figcaption>
-            {listImage.caption}
-            <span>PHOTO</span>:{listImage.byline}
-          </figcaption>
+          {isArticleView && (
+            <figcaption>
+              {listImage.caption}
+              <span>PHOTO</span>:{listImage.byline}
+            </figcaption>
+          )}
         </figure>
       )}
-      <p className="Article__authors">
-        {authors[0] && authors[0].byline}
-        <br />
-        <span>PUBLISHED: {publishingTime}</span>
-        <span>UPDATED: {updateTime}</span>
-      </p>
-      <p className="preamble">{preamble}</p>
-      {body && body.map(content => <p>{content.html}</p>)}
+      {/* title should be below image when not in article view */}
+      {!isArticleView && (
+        <header className="Article__header">
+          <h2 className={articleTitle}>{article.title}</h2>
+        </header>
+      )}
+      {!isList && (
+        <Fragment>
+          <p className="Article__authors">
+            {authors[0] && authors[0].byline}
+            <br />
+            <span>{publishingTime}</span>
+            <span>{updateTime}</span>
+          </p>
+          <p className="preamble">{preamble}</p>
+          {body && body.map(content => <p>{content.html}</p>)}
+        </Fragment>
+      )}
     </article>
   );
 };
