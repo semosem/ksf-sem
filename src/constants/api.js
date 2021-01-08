f17a1a9bfd04fac55b1c2a9e2c6da4327cde7d6d
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-08 11:48:09 +0200

    heroku setup

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = "https://lettera.api.ksfmedia.fi/v3";
 
 const LETTERA_FRONTPAGE_URL = `${BASE_URL}/frontpage`;
 const LETTERA_MOSTREAD_URL = `${BASE_URL}/mostread`;
