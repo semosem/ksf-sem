bd6c6abbe211d3593b20171e41092a4710b6cfeb
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-10 17:55:19 +0200

    premium article previews

---
+++ src/components/Article/Preview.js
@@ -0,0 +1,51 @@
+import { Fragment } from "react";
+import parse from "html-react-parser";
+import Body from "./Body";
+
+const Preview = ({ preview, getFormatedTime }) => {
+  if (!preview) return;
+  const {
+    publishingTime,
+    updateTime,
+    listImage,
+    tags,
+    preamble,
+    body,
+    authors,
+    title
+  } = preview;
+  console.log({ preview });
+  return (
+    <article className="Article">
+      {tags && tags[0] && <a href="">{tags[0].toUpperCase()}</a>}
+      {/* article main image*/}
+      {/* title should be above image when component is used on article view */}
+      <header className="Article__header">
+        <h2 className="title">{title}</h2>
+      </header>
+      {listImage && (
+        <figure className="Article__image">
+          <img src={listImage.url} alt="article main thumb" />
+          <figcaption>
+            {listImage.caption}
+            <span>PHOTO:{listImage.byline}</span>
+          </figcaption>
+        </figure>
+      )}
+      <Fragment>
+        <p className="Article__authors">
+          <b>{authors[0] && authors[0].byline}</b>
+          <br />
+          <span>{getFormatedTime(publishingTime)} </span>
+          <span>UPDATED {getFormatedTime(updateTime)}</span>
+        </p>
+        <p className="Article__preamble">{preamble}</p>
+        <section className="Article__body">
+          {body && <Body body={body} />}
+        </section>
+      </Fragment>
+    </article>
+  );
+};
+
+export default Preview;
