ee7746b0da17de7e211dd46a11baa18b415a5e80
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-10 17:55:49 +0200

    separate article body to own component and some style improvements

--- src/components/Article/Body.js
+++ src/components/Article/Body.js
@@ -1,5 +1,42 @@
-const ArticleBody = content => {
-  return <p>{content}</p>;
+import parse from "html-react-parser";
+
+const ArticleBody = ({ body }) => {
+  if (!body) return;
+  return body.map(content => {
+    const { headline, html, box, image } = content;
+    if (image) {
+      return (
+        <figure className="bodyImage">
+          <img src={image.url} alt="article main thumb" />
+          <figcaption>
+            {image.caption}
+            <span>PHOTO:{image.byline}</span>
+          </figcaption>
+        </figure>
+      );
+    } else if (headline) {
+      return <h3 className="preamble">{parse(headline)}</h3>;
+    } else if (box) {
+      console.log({ box });
+
+      return (
+        <div className="box">
+          <header>
+            <h4>{box.type}</h4>
+            <h2>{box.title}</h2>
+          </header>
+          <h2>{box.headline}</h2>
+          <ul>
+            {box.content.map(boxContent => {
+              return <li className="box_contnet">{parse(boxContent)}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <p className="preamble">{html && parse(html)}</p>;
+    }
+  });
 };
 
 export default ArticleBody;
