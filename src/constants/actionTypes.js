a771107e00f8a3f0d829814b5a583e683425a4e8
sem.gebresilassie@gmail.com <sem@adlaunch.com>
2021-01-11 08:17:19 +0200

    more action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,4 +1,6 @@
 export const FECTH_USER = "FECTH_USER";
 export const FECTH_ALL_ARTICLES = "FECTH_ALL_ARTICLES";
 export const LOAD_ARTICLE = "LOAD_ARTICLE";
+export const USER_LOADING = "USER_LOADING";
 export const USER_RECEIVE = "USER_RECEIVE";
+export const USER_FETCH_FAIL = "USER_FETCH_FAIL";
